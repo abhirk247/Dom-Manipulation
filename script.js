7307b68f810e4cade32dc10c24a37814fd76ca7d
Abhishek Anand <abhirk247@gmail.com>
2022-06-18 20:52:59 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// const body=document.body
+
+// body.append("Hello world","Bye")
+// body.appendChild("Second world")
+
+// const div=document.createElement('div')
+// div.innerText="Adding new string"
+// div.textContent="<strong>hello world</strong>"
+// div.innerHTML="<strong>hello world</strong>"
+// body.append(div)
+
+// const div=document.querySelector('div')
+// console.log(div.textContent)
+// console.log(div.innerText)
+
+// const strong=document.createElement("strong")
+// strong.innerText="Hello world 2"
+// div.append(strong)
+// div.append(div)
+
+const body=document.body
+const div=document.querySelector('div')
+const spanHi=document.querySelector('#hi')
+const spanBye=document.querySelector('#bye')
+
+// spanBye.remove()
+// div.append(spanBye)
+
+// div.removeChild(spanHi)
+
+// console.log(spanHi.getAttribute("id"))
+
+// console.log(spanHi.title)
+// console.log(spanHi.setAttribute('title',"newdd"))
+
+// spanHi.id= "mssdd"
+
+// spanHi.removeAttribute("title")
+
+// console.log(spanHi.dataset.test)
+// console.log(spanHi.dataset.longerName)
+
+// spanHi.dataset.newName= "hi"
+
+// spanHi.classList.add("new class")
+// spanHi.classList.remove("hi1")
+// spanHi.classList.toggle("hi3",true)
+
+// spanHi.style.color="red"
+spanHi.style.backgroundColor="blue"
